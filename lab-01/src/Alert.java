43dbae7ae06f9e098ba15939d77f6203bd82f01d
akimbodev <67482112+akimbodev@users.noreply.github.com>
2023-02-02 16:35:22 +0000

    completed lab 1

---
+++ lab-01/src/Alert.java
@@ -0,0 +1,6 @@
+public class Alert {
+    public static void main (String[] args) {
+        System.out.println(" An Emergency Broadcast I created my second program");
+        System.out.println("by your name");
+    }
+}
