fb75e13a19d1269b6502c1d9dfd233b2b00d1878
akimbodev <67482112+akimbodev@users.noreply.github.com>
2023-02-03 16:40:55 +0000

    moved files

---
+++ lab-02/Initials.java
@@ -0,0 +1,18 @@
+public class Initials {
+
+    public static final String letters = """
+            #########   #########   #########
+            #########   #########   #########
+            ###   ###   ###   ###   ###   ###
+            ###   ###   ###   ###   ###   ###
+            #########   #########   ###   ###
+            #########   #########   ###   ###
+            ###   ###   ###   ###   #########
+            ###   ###   ###   ###   #########
+            """;
+
+    public static void main(String[] args) {
+        System.out.println(letters);
+    }
+
+}
