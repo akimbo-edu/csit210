3f62a5937b4f92baa6d2682812a5de8ce7e76f3d
akimbodev <aolenik04@gmail.com>
2023-02-06 21:13:46 -0500

    changed constants to uppercase

--- lab-04/Calories.java
+++ lab-04/Calories.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Calories {
 
-    private static final int cookiesPerBag = 40;
-    private static final int servingsPerBag = 10;
-    private static final int caloriesPerServing = 300;
+    private static final int COOKIES_PER_BAG = 40;
+    private static final int SERVINGS_PER_BAG = 10;
+    private static final int CALORIES_PER_SERVING = 300;
 
-    private static final double servingsPerCookie = (double) servingsPerBag / cookiesPerBag;
-    private static final double caloriesPerCookie = (double) servingsPerCookie * caloriesPerServing;
+    private static final double SERVINGS_PER_COOKIE = (double) SERVINGS_PER_BAG / COOKIES_PER_BAG;
+    private static final double CALORIES_PER_COOKIE = (double) SERVINGS_PER_COOKIE * CALORIES_PER_SERVING;
     
     public static void main(String[] args) {
 
@@ -21,7 +21,7 @@ public class Calories {
         }
     
         int cookiesEaten = scanner.nextInt();
-        double caloriesConsumed = cookiesEaten * caloriesPerCookie;
+        double caloriesConsumed = cookiesEaten * CALORIES_PER_COOKIE;
         System.out.println(String.format("You've consumed %,.2f calories.", caloriesConsumed));
         
         scanner.close();
