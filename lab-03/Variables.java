f661247bdce06374d03d3bca269e08d6bcf3640a
akimbodev <67482112+akimbodev@users.noreply.github.com>
2023-02-08 13:53:27 +0000

    formatting

--- lab-03/Variables.java
+++ lab-03/Variables.java
@@ -1,11 +1,19 @@
 public class Variables {
 
-    private static final String contactName = "Alexander Olenik";
-    private static final int contactAge = 19;
-    private static final double annualPay = 1000000000.0;
+    private static final String CONTACT_NAME = "Alexander Olenik";
+    private static final int CONTACT_AGE = 19;
+    private static final double ANNUAL_PAY = 100000000.0;
 
     public static void main(String[] args) {
-        System.out.println(String.format("My name is %s, my age is %d and I hope to earn $%,.1f per year.", contactName, contactAge, annualPay));
+        System.out.println(String.format(
+            "My name is %s, my age is %d and I hope to earn $%,.1f per year.", 
+            CONTACT_NAME, 
+            CONTACT_AGE, 
+            ANNUAL_PAY));
+   
     }
     
+
+
 }
+
