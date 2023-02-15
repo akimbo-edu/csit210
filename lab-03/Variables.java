52de57824ff3c9e419ffce92d4e61bc223f31fdf
akimbodev <67482112+akimbodev@users.noreply.github.com>
2023-02-15 13:28:09 +0000

    updated to reflect new lab instructions

--- lab-03/Variables.java
+++ lab-03/Variables.java
@@ -1,19 +1,20 @@
+import java.text.NumberFormat;
+
 public class Variables {
+
+    // Must use NumberFormat class
 
     private static final String CONTACT_NAME = "Alexander Olenik";
     private static final int CONTACT_AGE = 19;
     private static final double ANNUAL_PAY = 100000000.0;
 
     public static void main(String[] args) {
-        System.out.println(String.format(
-            "My name is %s, my age is %d and I hope to earn $%,.1f per year.", 
-            CONTACT_NAME, 
-            CONTACT_AGE, 
-            ANNUAL_PAY));
-   
-    }
-    
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        String fmtPay = numberFormat.format(ANNUAL_PAY);
 
+        System.out.println("My name is " + CONTACT_NAME +
+                ", my age is " + CONTACT_AGE + " and I hope to earn " +
+                fmtPay + " per year.");
+    }
 
 }
-
